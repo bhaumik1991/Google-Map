3de9f8ce028f0122e023be29364fbe5cc0b1d9b2
arunava-indium <arunava.b@indiumsoft.com>
2022-09-14 17:42:00 +0530

    google map flutter

---
+++ lib/googlemap_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+
+import 'package:location/location.dart';
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+  LatLng? _latLng;
+
+  CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Future<void> getCurrentLocation() async{
+    Location location = Location();
+
+    bool _serviceEnabled = true;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if(!_serviceEnabled){
+      _serviceEnabled = await location.requestService();
+      if(!_serviceEnabled){
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if(_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await location.requestPermission();
+      if(_permissionGranted != PermissionStatus.granted){
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+    _latLng = LatLng(_locationData.latitude!, _locationData.longitude!);
+    print(_latLng);
+
+    _kGooglePlex = CameraPosition(
+      target: _latLng!,
+      zoom: 14.4746
+    );
+    setState(() {});
+
+  }
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: GoogleMap(
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          markers: <Marker>{
+            _setMarker()
+          },
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _goToTheLake,
+          label: Text('To the lake!'),
+          icon: Icon(Icons.directions_boat),
+        ),
+      ),
+    );
+  }
+
+  _setMarker(){
+    return Marker(
+        markerId: MarkerId("marker_1"),
+        icon: BitmapDescriptor.defaultMarker,
+        position: LatLng(37.42796133580664, -122.085749655962)
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
